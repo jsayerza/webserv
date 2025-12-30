0f1437d59100e1234e928b7674e4f16dc8edf162
Raúl Manzanas Trillo <genyo_liceo@hotmail.com>
2025-12-30 09:24:36 +0000

    init

---
+++ Server.hpp
@@ -0,0 +1,34 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include <vector>
+# include <map>
+# include <string>
+# include <poll.h>
+//# include "ServerConfig.hpp"
+
+class Request;
+class ServerConfig;
+
+class Server {
+private:
+    std::vector<struct pollfd>  _fds;
+    std::vector<ServerConfig>   _configs; 
+    std::map<int, std::string>  _clientBuffers;
+
+    void setupSockets();           // socket(), bind(), listen()
+    void acceptNewConnection();    // accept()
+    void readFromClient(int fd);   // recv() -> Leer y guardar bytes
+    void sendToClient(int fd);     // send() -> Envía la respuesta de Sayer
+
+public:
+        Server();                                   // 1. Constructor por defecto
+        Server(const std::vector<ServerConfig> &configs); //constructor con config
+        Server(const Server &other);                  // 2. Constructor de copia
+        Server &operator=(const Server &other);       // 3. Operador de asignación
+        ~Server();                                  // 4. Destructor
+
+    void run();
+};
+
+#endif
